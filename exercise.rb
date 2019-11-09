8278090ef4633ba152100db8e7f4be48c1a25663
Kristy Nelder <kristy.nelder@outlook.com>
2019-11-09 14:34:25 +1000

    added options to user

---
+++ exercise.rb
@@ -0,0 +1,18 @@
+# Requirements
+# loop
+# print options
+# be able to quit
+# takes 2 values
+#  -sum, subtract, multiply, divide and square
+
+loop do
+    puts "options"
+    puts "1. Sum"
+    puts "2. Subtract"
+    puts "3. Divide"
+    puts "4. Multiply"
+    puts "5. Square"
+    puts "q. Quit"
+    
+end
+
